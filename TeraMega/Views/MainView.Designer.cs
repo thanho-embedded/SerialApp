fcd12446a6a84dc17e05827bafb577b8010961d4
thanho <thanho.dut@gmail.com>
2023-06-09 14:07:41 +0700

    Changed solution name

--- TeraMega/Views/MainView.Designer.cs
+++ TeraMega/Views/MainView.Designer.cs
@@ -28,22 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thread 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Thread 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Thread 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Thread 4";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(131, 68);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(566, 13);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Tag = "PROCESS_1";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(131, 124);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(566, 13);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 1;
+            this.progressBar2.Tag = "PROCESS_2";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(131, 177);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(566, 13);
+            this.progressBar3.Step = 1;
+            this.progressBar3.TabIndex = 1;
+            this.progressBar3.Tag = "PROCESS_3";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(131, 229);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(566, 13);
+            this.progressBar4.Step = 1;
+            this.progressBar4.TabIndex = 1;
+            this.progressBar4.Tag = "PROCESS_4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(703, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Tag = "PERCENT_1";
+            this.label5.Text = "0 %";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(703, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Tag = "PERCENT_2";
+            this.label6.Text = "0 %";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(703, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Tag = "PERCENT_3";
+            this.label7.Text = "0 %";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(703, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Tag = "PERCENT_4";
+            this.label8.Text = "0 %";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(284, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(218, 25);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Multiple Threadings Test";
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Controls.Add(this.label9);
+            this.MainPanel.Controls.Add(this.label8);
+            this.MainPanel.Controls.Add(this.progressBar4);
+            this.MainPanel.Controls.Add(this.label5);
+            this.MainPanel.Controls.Add(this.progressBar3);
+            this.MainPanel.Controls.Add(this.label6);
+            this.MainPanel.Controls.Add(this.progressBar2);
+            this.MainPanel.Controls.Add(this.label7);
+            this.MainPanel.Controls.Add(this.progressBar1);
+            this.MainPanel.Controls.Add(this.label2);
+            this.MainPanel.Controls.Add(this.label4);
+            this.MainPanel.Controls.Add(this.label3);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(783, 278);
+            this.MainPanel.TabIndex = 2;
             // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 397);
+            this.ClientSize = new System.Drawing.Size(783, 278);
+            this.Controls.Add(this.MainPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainView";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel MainPanel;
     }
 }
--- TeraMega/Views/MainView.cs
+++ TeraMega/Views/MainView.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TeraMega.Services;
@@ -15,25 +16,171 @@ namespace TeraMega.Views
     public partial class MainView : Form
     {
         //...Fields
-        private Rs232Service Rs232Handle;
+        private Rs232Service rs232Handle;
+
+        //...Threadings
+        const string ROOT_THREAD_NAME = "MAIN_VIEW_THREAD_";
+        const string THREAD_1ST_NAME = "MAIN_VIEW_THREAD_1";
+        const string THREAD_2ND_NAME = "MAIN_VIEW_THREAD_2";
+        const string THREAD_3RD_NAME = "MAIN_VIEW_THREAD_3";
+        const string THREAD_4TH_NAME = "MAIN_VIEW_THREAD_4";
+
+        private Thread[] threadHandles = null;
+        private bool isThreadsLoop = true;
+
+        private int[] percentages = null;
+
         public MainView()
         {
             InitializeComponent();
 
             this.FormClosing += (s, e) =>
             {
+                ThreadsJoin();
                 Application.Exit();
             };
 
-            Rs232Handle = new Rs232Service("COM3", 115200);
-            if (Rs232Handle.Serial.Connect() != -1)
+            rs232Handle = new Rs232Service("COM3", 115200);
+            if (rs232Handle.Serial.Connect() != -1)
             {
                 MessageBox.Show("Open Serial Port is successful!");
             }
             else
             {
                 MessageBox.Show("Failed to open serial port!");
-            }          
+            }
+
+            ThreadInitialize(4);
+        }
+
+        private void ThreadInitialize(int count)
+        {
+            threadHandles = new Thread[count];
+            percentages = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                threadHandles[i] = ThreadsStart($"{ROOT_THREAD_NAME}{i+1}", ThreadCallback);               
+            }
+        }
+        private Thread ThreadsStart(string name, Action callback)
+        {
+            Thread handle = null;
+
+            handle = new Thread(new ThreadStart(callback));
+            handle.Name = name;
+            handle.Start();
+
+            return handle;
+        }
+        private void ThreadsJoin() 
+        {
+            isThreadsLoop = false;
+            for (int i = 0; i < threadHandles.Length;i++)
+            {
+                threadHandles[i].Join();
+            }
+        }
+        private void ThreadCallback()
+        {
+            while(isThreadsLoop)
+            {
+                switch (Thread.CurrentThread.Name)
+                {
+                    case THREAD_1ST_NAME:
+                        {
+                            SearchForUpdateProcessBarProperty("PROCESS_1", percentages[0]);
+                            SearchForUpdateLabelProperty("PERCENT_1", percentages[0].ToString() + " %");
+                            PercentageSimulation(0);
+                        }
+                        break;
+                    case THREAD_2ND_NAME:
+                        {
+                            SearchForUpdateProcessBarProperty("PROCESS_2", percentages[1]);
+                            SearchForUpdateLabelProperty("PERCENT_2", percentages[1].ToString() + " %");
+                            PercentageSimulation(1);
+                        }
+                        break;
+                    case THREAD_3RD_NAME:
+                        {
+                            SearchForUpdateProcessBarProperty("PROCESS_3", percentages[2]);
+                            SearchForUpdateLabelProperty("PERCENT_3", percentages[2].ToString() + " %");
+                            PercentageSimulation(2);
+                        }
+                        break;
+                    case THREAD_4TH_NAME:
+                        {
+                            SearchForUpdateProcessBarProperty("PROCESS_4", percentages[3]);
+                            SearchForUpdateLabelProperty("PERCENT_4", percentages[3].ToString() + " %");
+                            PercentageSimulation(3);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                Thread.Sleep(1);
+            }
+        }
+        //...Updated control properties
+        private void PercentageSimulation(int index)
+        {
+            if (percentages[index] < 100) percentages[index]++; else percentages[index] = 0;
+        }
+        private void SearchForUpdateProcessBarProperty(string tag, int val)
+        {
+            foreach (Control control in MainPanel.Controls)
+            {
+                if (control is ProgressBar ctrl)
+                {
+                    if ((string)ctrl.Tag == tag)
+                    {
+                        UpdateProcessBarProperty(ctrl, tag, val);
+                    }
+                }
+            }
+        }
+        private void SearchForUpdateLabelProperty(string tag, string val)
+        {
+            foreach (Control control in MainPanel.Controls)
+            {
+                if (control is Label ctrl)
+                {
+                    if ((string)ctrl.Tag == tag)
+                    {
+                        UpdateLabelProperty(ctrl, tag, val);
+                    }
+                }
+            }
+        }
+
+        private void UpdateLabelProperty(Label ctrl, string tag, string val)
+        {
+            if ((string)ctrl.Tag == tag)
+            {
+                if (ctrl.InvokeRequired)
+                {
+                    ctrl.Invoke(new Action(() => UpdateLabelProperty(ctrl, tag, val)));
+                }
+                else
+                {
+                    ctrl.Text = val;
+                }
+            }
+        }
+
+        private void UpdateProcessBarProperty(ProgressBar ctrl, string tag, int val)
+        {
+            if ((string)ctrl.Tag == tag)
+            {
+                if (ctrl.InvokeRequired)
+                {
+                    ctrl.Invoke(new Action(() => UpdateProcessBarProperty(ctrl, tag, val)));
+                }
+                else
+                {
+                    ctrl.Value = val;
+                }
+            }
         }
     }
 }
